80486615fb1b837f6e74ffd35322e621d6d54be8
MozartMongi <saya.mozart@gmail.com>
2024-11-12 22:25:18 +0700

    add useForm

--- src/_metronic/partials/widgets/lists/ListsWidget1.jsx
+++ src/_metronic/partials/widgets/lists/ListsWidget1.jsx
@@ -1,18 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react'
 import {KTIcon} from '../../../helpers'
-import {Dropdown1} from '../../content/dropdown/Dropdown1'
-import { generateText } from '../../../../app/modules/apps/user-management/users-list/core/_requestOpenAi'
+import { useForm } from 'react-hook-form';
 
-// type Props = {
-//   className: string,
-//   handleGenerateContent: Function,
-//   onChangeSecondParty: Function,
-//   onChangeFirstParty: Function,
-//   onChangePrompt: Function
-// }
+
 
 const ListsWidget1 = ({className, purpose, handleGenerateContent, onChangePrompt, object, onChangeObject, isLoading}) => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onGenerateContent = (data) => {
+    console.log('DATA >>', data)
+  }
 
   
 
@@ -34,27 +32,29 @@ const ListsWidget1 = ({className, purpose, handleGenerateContent, onChangePrompt
         {/* begin::Item */}
         <div className='align-items-center mb-7'>
           <div>
-            <label className="form-label required">Pihak 1</label>
+            <label className="form-label required">Identitas Pihak 1</label>
             <div className='d-flex align-items-center'>
               <div className='symbol symbol-50px me-5'>
-              <span className='symbol-label bg-light-warning'>
-                <KTIcon iconName='pencil' className='fs-2x text-warning' />
-              </span>
+                <span className='symbol-label bg-light-warning'>
+                  <KTIcon iconName='pencil' className='fs-2x text-warning' />
+                </span>
+              </div>
+              <input
+              {...register("firstParty", {required: true})}
+              // value={object.firstParty}
+              // onChange={(e) => onChangeObject(prev => ({...prev, firstParty: e.target.value}))}
+                type="text"
+                className="form-control"
+                placeholder="nama, alamat, dan nomor identitas"
+              />
             </div>
-            <input
-            value={object.firstParty}
-            onChange={(e) => onChangeObject(prev => ({...prev, firstParty: e.target.value}))}
-              type="text"
-              className="form-control"
-              placeholder="nama Pihak 1..."
-            />
-            </div>
+            {errors.firstParty && <span>Wajib diisi!</span>}
           </div>
         </div>
         {/* end::Item */}
         <div className='align-items-center mb-7'>
           <div>
-            <label className="form-label required">Pihak 2</label>
+            <label className="form-label required">Identitas Pihak 2</label>
             <div className='d-flex align-items-center'>
               <div className='symbol symbol-50px me-5'>
               <span className='symbol-label bg-light-success'>
@@ -62,18 +62,20 @@ const ListsWidget1 = ({className, purpose, handleGenerateContent, onChangePrompt
               </span>
             </div>
             <input
-              value={object.secondParty}
-              onChange={(e) => onChangeObject(prev => ({...prev, secondParty: e.target.value}))}
+              {...register("secondParty", {required: true})}
+              // value={object.secondParty}
+              // onChange={(e) => onChangeObject(prev => ({...prev, secondParty: e.target.value}))}
               type="text"
               className="form-control"
-              placeholder="nama Pihak 2..."
+              placeholder="nama, alamat, dan nomor identitas"
             />
             </div>
+            {errors.secondParty && <span>Wajib diisi!</span>}
           </div>
         </div>
         <div className='align-items-center mb-7'>
           <div>
-            <label className="form-label required">Tujuan</label>
+            <label className="form-label required">Pokok Perjanjian</label>
             <div className='d-flex align-items-center'>
               <div className='symbol symbol-50px me-5'>
               <span className='symbol-label bg-light-info'>
@@ -81,18 +83,41 @@ const ListsWidget1 = ({className, purpose, handleGenerateContent, onChangePrompt
               </span>
             </div>
             <input
-              value={purpose}
+              {...register("purpose", {required: true})}
+              // value={purpose}
+              // onChange={(e) => onChangePrompt(e.target.value)}
               type="text"
               className="form-control"
               placeholder="Jual Beli Mobil..."
-              onChange={(e) => onChangePrompt(e.target.value)}
             />
             </div>
+            {errors.purpose && <span>Wajib diisi!</span>}
           </div>
         </div>
         <div className='align-items-center mb-7'>
           <div>
-            <label className="form-label required">Durasi</label>
+            <label className="form-label required">Tempat dan tanggal pembuatan perjanjian</label>
+            <div className='d-flex align-items-center'>
+              <div className='symbol symbol-50px me-5'>
+              <span className='symbol-label bg-light-info'>
+                <KTIcon iconName='message-text-2' className='fs-2x text-info' />
+              </span>
+            </div>
+            <input
+              {...register("placeAndDate", {required: true})}
+              // value={purpose}
+              // onChange={(e) => onChangePrompt(e.target.value)}
+              type="text"
+              className="form-control"
+              placeholder="Jakarta, 14 Agustus 1945"
+            />
+            </div>
+            {errors.placeAndDate && <span>Wajib diisi!</span>}
+          </div>
+        </div>
+        <div className='align-items-center mb-7'>
+          <div>
+            <label className="form-label required">Jangka waktu pelaksanaan</label>
             <div className='d-flex align-items-center'>
               <div className='symbol symbol-50px me-5'>
               <span className='symbol-label bg-light-warning'>
@@ -100,13 +125,15 @@ const ListsWidget1 = ({className, purpose, handleGenerateContent, onChangePrompt
               </span>
             </div>
             <input
-            value={object.duration}
-            onChange={(e) => onChangeObject(prev => ({...prev, duration: e.target.value}))}
+              {...register('duration', {required: true})}
+            // value={object.duration}
+            // onChange={(e) => onChangeObject(prev => ({...prev, duration: e.target.value}))}
               type="text"
               className="form-control"
               placeholder="durasi perjanjian"
             />
             </div>
+            {errors.duration && <span>Wajib diisi!</span>}
           </div>
         </div>
         <div className='align-items-center mb-7'>
@@ -119,13 +146,15 @@ const ListsWidget1 = ({className, purpose, handleGenerateContent, onChangePrompt
               </span>
             </div>
             <input
-            value={object.obj}
-            onChange={(e) => onChangeObject(prev => ({...prev, obj: e.target.value}))}
+              {...register('object', {required: true})}
+            // value={object.obj}
+            // onChange={(e) => onChangeObject(prev => ({...prev, obj: e.target.value}))}
               type="text"
               className="form-control"
               placeholder="nama objek"
             />
             </div>
+            {errors.object && <span>Wajib diisi!</span>}
           </div>
         </div>
         <div className='align-items-center mb-7'>
@@ -138,13 +167,15 @@ const ListsWidget1 = ({className, purpose, handleGenerateContent, onChangePrompt
               </span>
             </div>
             <input
-            value={object.cost}
-            onChange={(e) => onChangeObject(prev => ({...prev, cost: e.target.value}))}
+              {...register('cost', {required: true})}
+            // value={object.cost}
+            // onChange={(e) => onChangeObject(prev => ({...prev, cost: e.target.value}))}
               type="text"
               className="form-control"
               placeholder="Biaya yang ditimbulkan"
             />
             </div>
+            {errors.cost && <span>Wajib diisi!</span>}
           </div>
         </div>
         <div className='align-items-center mb-7'>
@@ -157,8 +188,9 @@ const ListsWidget1 = ({className, purpose, handleGenerateContent, onChangePrompt
               </span>
             </div>
             <input
-            value={object.tnc}
-            onChange={(e) => onChangeObject(prev => ({...prev, tnc: e.target.value}))}
+              {...register('tnc')}
+            // value={object.tnc}
+            // onChange={(e) => onChangeObject(prev => ({...prev, tnc: e.target.value}))}
               type="text"
               className="form-control"
               placeholder="ketentuan yang diberlakukan"
@@ -167,7 +199,7 @@ const ListsWidget1 = ({className, purpose, handleGenerateContent, onChangePrompt
           </div>
         </div>
         <div className=''>
-          <a onClick={handleGenerateContent} href="#" className="btn btn-primary w-100">{isLoading ? 'Loading...' : 'Proses'}</a>
+          <a onClick={handleSubmit(onGenerateContent)} href="#" className="btn btn-primary w-100">{isLoading ? 'Loading...' : 'Proses'}</a>
         </div>
       </div>
       {/* end::Body */}
